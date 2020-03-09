9540f149e5805f5b2ce7ab1a2175c5966101abb3
Goutham Rangarajan <grangarajan@ana-data.com>
2020-03-08 21:26:42 -0400

    fixed typo error in config

--- nuxt-materialize/nuxt.config.js
+++ nuxt-materialize/nuxt.config.js
@@ -14,7 +14,7 @@ export default {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto&display=swap' },
-      { rel: 'icon', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
     ]
   },
   /*
