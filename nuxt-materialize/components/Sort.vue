0043f33a964d54b7e1f05b00564c0f31c8cdae7e
Goutham Rangarajan <grangarajan@ana-data.com>
2020-03-15 14:47:18 -0400

    sorting in albums page

---
+++ nuxt-materialize/components/Sort.vue
@@ -0,0 +1,40 @@
+<template>
+   <transition :name="'rotate-'+order" mode="out-in">
+      <i class="material-icons tiny black-text" v-if="order=='asc'" :key="1">arrow_upward</i>
+      <i class="material-icons tiny black-text" v-else :key="2"> arrow_downward</i>
+  </transition>
+</template>
+<script>
+export default {
+  props:{
+    order:{
+      type:String,
+      required:true,
+      default:'asc',
+      validator: function (value) {
+        return ['asc', 'desc'].indexOf(value) !== -1
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.rotate-asc-enter-active{
+  transition:all 0.3s
+}
+.rotate-asc-enter,
+.rotate-asc-leave-to{
+  opacity:0;
+  transform:rotate(180deg);
+}
+
+.rotate-desc-enter-active{
+  transition:all 0.3s;
+}
+
+.rotate-desc-enter,
+.rotate-desc-leave-to{
+  opacity:0;
+  transform:rotate(-180deg);
+}
+</style>
--- nuxt-materialize/pages/albums.vue
+++ nuxt-materialize/pages/albums.vue
@@ -6,13 +6,37 @@
       <table class="striped responsive-table">
         <thead class="white-text">
             <tr  class="light-blue accent-4">
-                <th rowspan="2" class="center">Title</th>
-                <th colspan="3" class="center">User</th>
+                <th rowspan="2" @click="changeSort(0)" class="center">Title
+                  <transition name="fade">
+                    <span class="right" v-if="sortInfo.column==0">
+                        <sort :order="sortInfo.order"/>
+                    </span>
+                  </transition>
+                </th>
+                <th colspan="3" class="center" style="cursor:default">User</th>
             </tr>
             <tr  class="light-blue accent-4">
-              <th class="center">Name</th>
-              <th class="center">User Name</th>
-              <th class="center">Email</th>
+              <th @click="changeSort(1)">Name
+                <transition name="fade">
+                  <span class="right" v-if="sortInfo.column==1">
+                      <sort :order="sortInfo.order"/>
+                  </span>
+                </transition>
+              </th>
+              <th @click="changeSort(2)">User Name
+                <transition name="fade">
+                  <span class="right" v-if="sortInfo.column==2">
+                      <sort :order="sortInfo.order"/>
+                  </span>
+                </transition>
+              </th>
+              <th @click="changeSort(3)">Email
+                <transition name="fade">
+                  <span class="right" v-if="sortInfo.column==3">
+                      <sort :order="sortInfo.order"/>
+                  </span>
+                </transition>
+              </th>
             </tr>
           </thead>
           <transition-group name="records" tag="tbody">
@@ -45,6 +69,7 @@
 <script>
 import {mapState} from 'vuex'
 import pagination from '@/components/Pagination.vue'
+import sort from '@/components/Sort.vue'
 import userModalContent from '@/components/UserModalContent.vue'
 export default {
   transition:'slide',
@@ -57,114 +82,178 @@ export default {
             'content':'nuxt materialize jsonplaceholder albums'}
         ]
       }
-    },
-    data(){
-      return {pageInfo:{currPage:1,totalPages:0,pageLen:10},modalUserId:-1,unsubscribe:null}
-    },
-    components:{
-      pagination,
-      userModalContent
-    },
-    mounted(){
-      this.unsubscribe=this.$store.subscribe((mutation)=>{
-        if(mutation.type=='albums/setData'){
-            this.initializeMaterialize()
-          }
-      })
-      this.$store.dispatch('users/refresh')
-      this.$store.dispatch('albums/refresh')
-    },
-    destroyed(){
-      if(this.unsubscribe){
-        this.unsubscribe();
-      }
-    },
-    computed:{
-        ...mapState({
-            albums:state=>state.albums.data,
-            users:state=>state.users.data,
-            ftTxt:state=>state.searchTxt
-        }),
-        ftAlbums(){
-           var data= this.albums.map(el=>{
-                var user=this.users.filter(usEl=>usEl.id==el.userId)[0];
-                el.user=user;
-                return el;
-            }).filter(el=>{
-                if(this.ftTxt!=''){
-                    if( el.title.toLowerCase().includes(this.ftTxt.toLowerCase())||
-                        el.user.username.toLowerCase().includes(this.ftTxt.toLowerCase()) ||
-                        el.user.email.toLowerCase().includes(this.ftTxt.toLowerCase()) ||
-                        el.user.name.toLowerCase().includes(this.ftTxt.toLowerCase())
-                      )
-
-                      return true;
-                     else
-                      return false;
-                }
-                return true;
-            });
-            return data;
-        },
-        pgedAlbums(){
-            let endInd=(this.pageInfo.currPage*this.pageInfo.pageLen)
-            return this.ftAlbums.slice(endInd-this.pageInfo.pageLen,endInd)
-        },
-        paginationData(){
-          var display=this.pgedAlbums.length>0;
-          var end=(this.pageInfo.currPage*this.pageInfo.pageLen);
-          var start=end-this.pageInfo.pageLen;
-          if(end>this.ftAlbums.length){
-              end=this.ftAlbums.length;
-          }
-          var fromTo= (start+1)+'-'+end;
-          return {
-              fromTo,
-              isFirstPage:this.pageInfo.currPage==1,
-              isLastPage: this.pageInfo.currPage*this.pageInfo.pageLen >= this.ftAlbums.length,
-              length:this.pageInfo.pageLen,
-              display,
-              pgLenList:[10,25,50,100],
-              total:this.ftAlbums.length
-          };
+  },
+  data(){
+    return {pageInfo:{currPage:1,totalPages:0,pageLen:10},
+            sortInfo:{column:0,order:'asc'},
+            modalUserId:-1,unsubscribe:null}
+  },
+  components:{
+    pagination,
+    userModalContent,
+    sort
+  },
+  mounted(){
+    this.unsubscribe=this.$store.subscribe((mutation)=>{
+      if(mutation.type=='albums/setData'){
+          this.initializeMaterialize()
         }
-    },
-    methods:{
-      increasePage(){
-           var isLastPage= this.pageInfo.currPage*this.pageInfo.pageLen >= this.ftAlbums.length;
-            if(isLastPage==true){
-                return;
-            }
-            this.pageInfo.currPage++;
-        },
-        decreasePage(){
-            if(this.pageInfo.currPage==1){
-                return;
-            }
-            this.pageInfo.currPage--;
-        },
-        resetPage(){
-          this.pageInfo.currPage=1
-        },
-        initializeMaterialize(){
-           let elems = document.querySelectorAll('.modal');
-           let instances = M.Modal.init(elems, {});
-           elems = document.querySelectorAll('.tooltipped');
-           instances = M.Tooltip.init(elems, {});
-        }
-    },
-    watch:{
-      ftAlbums:'resetPage',
-      pgedAlbums(newVal,oldVal){
-          setTimeout(()=>{
-            this.initializeMaterialize()
-          },300)
-      }
+    })
+    this.$store.dispatch('users/refresh')
+    this.$store.dispatch('albums/refresh')
+  },
+  destroyed(){
+    if(this.unsubscribe){
+      this.unsubscribe();
     }
+  },
+  computed:{
+      ...mapState({
+          albums:state=>state.albums.data,
+          users:state=>state.users.data,
+          ftTxt:state=>state.searchTxt
+      }),
+      ftAlbums(){
+          var data= this.albums.map(el=>{
+              var user=this.users.filter(usEl=>usEl.id==el.userId)[0];
+              el.user=user;
+              return el;
+          }).filter(el=>{
+              if(this.ftTxt!=''){
+                  if( el.title.toLowerCase().includes(this.ftTxt.toLowerCase())||
+                      el.user.username.toLowerCase().includes(this.ftTxt.toLowerCase()) ||
+                      el.user.email.toLowerCase().includes(this.ftTxt.toLowerCase()) ||
+                      el.user.name.toLowerCase().includes(this.ftTxt.toLowerCase())
+                    )
+
+                    return true;
+                    else
+                    return false;
+              }
+              return true;
+          });
+          return data;
+      },
+      stAlbums(){
+        let sortColumnTranslation={
+          0:['title'],
+          1:['user','name'],
+          2:['user','username'],
+          3:['user','email']
+        }
+        return this.ftAlbums.sort((a,b)=>{
+          let translation=sortColumnTranslation[this.sortInfo.column]
+          if(this.sortInfo.order=='asc'){
+            if(translation.length==1){
+               if(a[translation[0]]>b[translation[0]])
+                 return 1
+               else if(a[translation[0]]<b[translation[0]])
+                return -1
+              else
+                return 0
+            }
+            else{
+              if(a[translation[0]][translation[1]]>b[translation[0]][translation[1]])
+                 return 1
+               else if(a[translation[0]][translation[1]]<b[translation[0]][translation[1]])
+                return -1
+              else
+                return 0
+            }
+          }
+          else{
+            if(translation.length==1){
+               if(a[translation[0]]<b[translation[0]])
+                 return 1
+               else if(a[translation[0]]>b[translation[0]])
+                return -1
+              else
+                return 0
+            }
+            else{
+              if(a[translation[0]][translation[1]]<b[translation[0]][translation[1]])
+                 return 1
+               else if(a[translation[0]][translation[1]]>b[translation[0]][translation[1]])
+                return -1
+              else
+                return 0
+            }
+          }
+        })
+      },
+      pgedAlbums(){
+          let endInd=(this.pageInfo.currPage*this.pageInfo.pageLen)
+          return this.stAlbums.slice(endInd-this.pageInfo.pageLen,endInd)
+      },
+      paginationData(){
+        var display=this.pgedAlbums.length>0;
+        var end=(this.pageInfo.currPage*this.pageInfo.pageLen);
+        var start=end-this.pageInfo.pageLen;
+        if(end>this.ftAlbums.length){
+            end=this.ftAlbums.length;
+        }
+        var fromTo= (start+1)+'-'+end;
+        return {
+            fromTo,
+            isFirstPage:this.pageInfo.currPage==1,
+            isLastPage: this.pageInfo.currPage*this.pageInfo.pageLen >= this.ftAlbums.length,
+            length:this.pageInfo.pageLen,
+            display,
+            pgLenList:[10,25,50,100],
+            total:this.ftAlbums.length
+        };
+      }
+  },
+  methods:{
+    increasePage(){
+          var isLastPage= this.pageInfo.currPage*this.pageInfo.pageLen >= this.ftAlbums.length;
+          if(isLastPage==true){
+              return;
+          }
+          this.pageInfo.currPage++;
+      },
+      decreasePage(){
+          if(this.pageInfo.currPage==1){
+              return;
+          }
+          this.pageInfo.currPage--;
+      },
+      resetPage(){
+        this.pageInfo.currPage=1
+      },
+      initializeMaterialize(){
+          let elems = document.querySelectorAll('.modal');
+          let instances = M.Modal.init(elems, {});
+          elems = document.querySelectorAll('.tooltipped');
+          instances = M.Tooltip.init(elems, {});
+      },
+      changeSort(col){
+        if(this.sortInfo.column==col)
+          if(this.sortInfo.order=='asc')
+            this.sortInfo.order='desc'
+          else
+            this.sortInfo.order='asc'
+        else{
+          this.sortInfo.column=col
+          this.sortInfo.order='asc'
+        }
+      }
+  },
+  watch:{
+    ftAlbums:'resetPage',
+    pgedAlbums(newVal,oldVal){
+        setTimeout(()=>{
+          this.initializeMaterialize()
+        },300)
+    }
+  }
 }
 </script>
 <style scoped>
 .z-depth-5{
   margin-bottom:1rem;
+}
+th{
+  cursor: pointer;
 }
 </style>
