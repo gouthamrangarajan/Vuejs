db1c414e2fae5ee7418dd891b41e6062262f94b6
Goutham Rangarajan <grangarajan@ana-data.com>
2020-06-05 20:32:28 -0400

    minor change in footer component

--- web-learnings/components/AppFooter.vue
+++ web-learnings/components/AppFooter.vue
@@ -1,11 +1,20 @@
 <template functional>
   <footer class="page-footer white">
       <div class="container">
-            <div class="center">
+             <div class="row">
+               <div class="col l6 s12">
                 <h5 class="cyan-text text-darken-1">About</h5>
                 <p class="grey-text text-darken-4">Collection of all video, document, blog learnings
                   covering topics in html, css frameworks, javascript.</p>
-            </div>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="cyan-text text-darken-1">Contact</h5>
+                <ul>
+                  <li><a class="grey-text text-darken-4" href="https://www.linkedin.com/in/gouthamrangarajan/"><i class="material-icons tiny">people</i>&nbsp;Linkedin</a></li>
+                  <li><a class="grey-text text-darken-4" href="mailTo:rgouthamraja@yahoo.com"><i class="material-icons tiny">email</i>&nbsp;Email</a></li>
+                </ul>
+              </div>
+             </div>
           </div>
   </footer>
 </template>
