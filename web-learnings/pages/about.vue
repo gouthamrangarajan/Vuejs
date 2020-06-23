119b04b0e59292f9adc8e233c6ede853ac96c3d4
goutham rangarajan <rgouthamraja@yahoo.com>
2020-06-23 12:23:37 -0400

    added few more pages like error,404 etc

--- web-learnings/pages/about.vue
+++ web-learnings/pages/about.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    about
+     <h5 class="teal-text text-darken-4">Collection of all video, document, blog learnings
+                  covering topics in html, css frameworks, javascript.</h5>
   </div>
 </template>
 <script>
 export default {
-  transition:'records'
+  transition:'fade'
 }
 </script>
---
+++ web-learnings/pages/error.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h5 class="red-text text-darken-1">Oops...Page not found</h5>
+  </div>
+</template>
+<script>
+export default {
+  transition:'fade'
+}
+</script>
