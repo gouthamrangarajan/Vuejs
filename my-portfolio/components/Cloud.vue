5e8aa96913935c29a7d8ae6289ee958855590cdb
Goutham Rangarajan <grangarajan@ana-data.com>
2020-07-25 15:08:03 -0400

    small adjustments in cloud project section

--- my-portfolio/components/Cloud.vue
+++ my-portfolio/components/Cloud.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex justify-center p-1">
         <div class="cloud flex flex-col justify-center bg-white py-4 px-6 rounded shadow">
-            <span class="text-xl text-orange-700">Cloud Hostings:</span>
+            <span class="text-xl text-orange-700">Cloud Projects:</span>
             <div v-for="cloud in category" :key="cloud.id">
                 <div class="p-2">
                     <span class="text-lg text-yellow-700 uppercase"> {{cloud.name}}</span>
@@ -9,7 +9,7 @@
                         <div v-for="(item,index) in cloud.collection" :key="index">
                             <template v-if="item.url">
                                 <a class="cursor-pointer underline text-blue-700 text-justify" @click="launch(item.url)">{{item.url}}</a>
-                                <p class="px-4 text-justify">{{item.description}}</p>
+                                <p class="px-4 text-justify text-sm">{{item.description}}</p>
                             </template>
                             <template v-if="item.other">
                                 <div v-for="(otherItem,otherindex) in item.other" :key="otherindex">
