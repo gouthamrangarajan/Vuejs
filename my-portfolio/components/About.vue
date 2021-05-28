8181fde5896c1b2d2cc416bd6bce44a30a46d1c1
Goutham Rangarajan <grangarajan@ana-data.com>
2021-05-28 08:24:46 -0400

    fixed width for about card

--- my-portfolio/components/About.vue
+++ my-portfolio/components/About.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="flex w-full items-center justify-center p-1">
-    <div class="flex flex-col items-start rounded px-6 py-4 shadow bg-white">
+    <div
+      class="
+        flex flex-col
+        items-start
+        rounded
+        px-6
+        py-4
+        shadow
+        bg-white
+        w-full
+        lg:w-9/12
+        xl:w-7/12
+      "
+    >
       <span class="text-teal-700 text-xl">About</span>
       <template v-if="info != ''">
         <p class="text-md">{{ info }}</p>
       </template>
       <template v-else>
-        <div class="animate-pulse w-64">
+        <div class="animate-pulse w-11/12">
           <div class="rounded bg-gray-400 h-4 w-full"></div>
         </div>
       </template>
