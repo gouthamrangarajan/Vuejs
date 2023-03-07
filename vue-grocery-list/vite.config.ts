73fd21ee494293ace32d77b22d6c4df146e22308
Goutham Rangarajan <grangarajan@ana-data.com>
2023-03-06 19:27:23 -0500

    RG PWA implementation

--- vue-grocery-list/vite.config.ts
+++ vue-grocery-list/vite.config.ts
@@ -7,7 +7,26 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Pages(), VitePWA()],
+  plugins: [
+    vue(),
+    Pages(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Grocery List',
+        short_name: 'Grocery List',
+        description: 'Grocery List',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
