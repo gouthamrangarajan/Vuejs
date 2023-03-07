086e59d7406670567300aa3f4d167406fd9eb5bd
Goutham Rangarajan <grangarajan@ana-data.com>
2023-03-06 20:50:21 -0500

    RG PWA changes

--- vue-grocery-list/vite.config.ts
+++ vue-grocery-list/vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     vue(),
     Pages(),
     VitePWA({
+      registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
         name: 'Grocery List',
